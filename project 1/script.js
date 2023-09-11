a7c32239f7b6965a0ed77d42dbcb4f1b87ced41c
shruti421 <103622541+shruti421@users.noreply.github.com>
2023-09-11 21:01:48 +0530

    Add files via upload

---
+++ project 1/script.js
@@ -0,0 +1,84 @@
+// let num1 = "19";
+// let num2 = '01302246757 ';
+
+// let fact = "My current age is ${num1} and my age after one year is ${num2}";
+// console.log(fact)
+
+// fact = `My current age is ${num1} and my age after one year is ${num2}`;
+
+// console.log(fact)
+
+// let age = 6; 
+// let drink = age>5 ? 'coffee':'milk';
+
+// console.log(drink)
+
+
+// let winningNumIs = 20;
+
+// let ans = prompt('Guess the number : ');
+
+// if (ans === winningNumIs){
+//     console.log('Your guess is right.')
+// }
+// else if (ans>winningNumIs){
+//     console.log('Your guess is too high')
+// }
+// else{
+//     console.log('Your guess is too low')
+// }
+
+
+// let day = 3;
+
+// switch (day){
+//     case 0:
+//         console.log('Sunday');
+//         break;
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thrusday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+//     default:
+//         console.log('Value error !!')
+
+// }
+
+// let num = 10;
+// let ans = 0;
+
+// while (num>0){
+//     ans = ans+num
+//     num--;
+// }
+// console.log(ans)
+
+// let mixed = [1,2,3]
+
+// // mixed = 12
+// console.log(mixed)
+// console.log(typeof mixed)
+// console.log(Array.isArray(mixed))
+
+let num1 = ['Apple','Mango','Grapes','Cherry','Banana'];
+let len = num1.length
+
+console.log(len)
+
+for (i=0;i<len;i++){
+    console.log(num1[i]);
+}
